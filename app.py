4948dffb6cdfadfbaad8cb42e746bd0f729a71cb
cybermito <cybermito@gmail.com>
2022-09-18 16:38:08 +0200

    Corriendo una apliación sencilla  - Hola Mundo

---
+++ app.py
@@ -0,0 +1,11 @@
+from distutils.log import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return 'Hola mundo, mi primera app Flask'
+
+if __name__ == '__main__':
+    app.run('localhost',5000, debug=True)
